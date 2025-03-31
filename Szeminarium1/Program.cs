e783915946f45387aaf1824f6a66bb437f2fdb00
Balazs01 <fbalazsf2004@gmail.com>
2025-03-31 22:00:02 +0300

    1.hiba elrontottam a pontokat tartalmazo tombot

--- Szeminarium1/Program.cs
+++ Szeminarium1/Program.cs
@@ -110,7 +110,7 @@ namespace Szeminarium1
             float[] vertexArray = new float[] {
                 -0.5f, -0.5f, 0.0f,
                 +0.5f, -0.5f, 0.0f,
-                 0.0f, +0.5f, 0.0f,
+                 0.0f,          //elrontottam a tombot
                  1f, 1f, 0f
             };
 
